dc4bffdf7ba491bc2a00a5b1a5a2150c340f675e
Evander Aimufua <eva707@usask.ca>
2024-05-26 22:56:46 -0600

    feat:initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,109 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [number, setNumber] = useState("");
+  const [name, setName] = useState("");
+  const [dateTime, setDateTime] = useState("");
+  const [description, setDescription] = useState("");
+  const [transactions, setTransactions] = useState([]);
+
+  useEffect(() => {
+    getTransactions().then(setTransactions);
+  }, []);
+
+  async function getTransactions() {
+    const url = process.env.REACT_APP_API_URL + "/transactions";
+    const response = await fetch(url);
+    return await response.json();
+  }
+  //handles new transactions and sends to the backend
+  function handleNewTransaction(event) {
+    event.preventDefault();
+    const url = process.env.REACT_APP_API_URL + "/transaction";
+    fetch(url, {
+      method: "POST",
+      headers: { "Content-type": "application/json" },
+      body: JSON.stringify({ number, name, description, dateTime }),
+    }).then((response) => {
+      response.json().then((json) => {
+        setNumber("");
+        setName("");
+        setDateTime("");
+        setDescription("");
+        console.log("result", json);
+      });
+    });
+  }
+  let balance = 0;
+  for (const transaction of transactions) {
+    balance = balance + transaction.number;
+  }
+  balance = balance.toFixed(2);
+  const fraction = balance.split(".")[1];
+  balance = balance.split(".")[0];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <div className="price-container">
+        <h1>
+          ${balance}
+          <span>.{fraction}</span>
+        </h1>
+      </div>
+      <form onSubmit={handleNewTransaction}>
+        <div className="input-container">
+          <input
+            type="text"
+            value={number}
+            onChange={(event) => setNumber(event.target.value)}
+            placeholder={"+/- Price"}
+          />
+          <input
+            type="text"
+            value={name}
+            onChange={(event) => setName(event.target.value)}
+            placeholder={"Name"}
+          />
+          <input
+            type="datetime-local"
+            value={dateTime}
+            onChange={(event) => setDateTime(event.target.value)}
+          />
+        </div>
+        <div className="description-box">
+          <input
+            type="text"
+            value={description}
+            onChange={(event) => setDescription(event.target.value)}
+            placeholder={"Description"}
+          />
+        </div>
+        <button type="submit"> Add New Transaction</button>
+      </form>
+
+      <div className="transactions">
+        {transactions.length > 0 &&
+          transactions.map((transaction) => (
+            <div className="transaction-container">
+              <div className="transactions-container-left">
+                <div className="item-name">{transaction.name}</div>
+                <div className="description">{transaction.description}</div>
+              </div>
+              <div className="transactions-container-right">
+                <div
+                  className={
+                    "price " + (transaction.number < 0 ? "red" : "green")
+                  }
+                >
+                  {transaction.number}
+                </div>
+                <div className="datetime">{transaction.dateTime}</div>
+              </div>
+            </div>
+          ))}
+      </div>
+    </main>
   );
 }
 
